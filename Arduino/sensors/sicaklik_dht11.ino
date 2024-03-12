6df9a371e6f72bdf0eae946665df800a210b7cb1
rahmielibol <64936670+rahmielibol@users.noreply.github.com>
2024-03-12 22:44:39 +0300

    Create sicaklik_dht11.ino

---
+++ Arduino/sensors/sicaklik_dht11.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>
+
+#define dhtPini 2
+#define dhtTipi DHT11
+
+DHT dht(dhtPini, dhtTipi);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  dht.begin();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  float sicaklik = dht.readTemperature();
+  float nem = dht.readHumidity();
+
+  Serial.print("Sicaklik :  " );
+  Serial.print(sicaklik);
+  Serial.println(" C" );
+  Serial.print("Nem : % " );
+  Serial.println(nem);
+  Serial.println("--- --- --- ---");
+  delay(1000);
+}
